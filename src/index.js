dd122011e3e6f3dc35ebc731606c156e39f4105a
justsicklife <justsicklife@gmail.com>
2021-01-06 12:38:37 +0900

    ie11 에서 가동되게 만들어봤는데 잘될지 모르겠음

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import "./index.css";
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
